688a37089c596fe6bb472f959a1cb5794ee977c6
MarianoBordon8 <bordon.marianooscar@gmail.com>
2023-11-14 00:12:43 -0300

    Segunda entrega

--- main.js
+++ main.js
@@ -1,53 +1,88 @@
+const fs = require('fs');
+
+
 class ProductManager{
     constructor() {
         this.products = []
-        this.id = 1
+        this.path = "productos.txt"
     }
-    getProducts(){
-        console.log(this.products)
+    static id = 0
+        async getProducts(){
+        let obtainProducts
+        const Existe = fs.existsSync(this.path)
+        if (!Existe) {
+            obtainProducts = []
+        } else {
+            obtainProducts = await fs.promises.readFile(this.path, "utf-8")
+            obtainProducts = JSON.parse(obtainProducts)
+        }
+        this.products = obtainProducts
+        return obtainProducts
     }
-    addProduct(title, description, price, thumbnail, code, stock){
+
+    async addProduct(title, description, price, thumbnail, code, stock){
         if (!title || !description || !price || !thumbnail || !code || !stock){
             console.log("faltan campos")
         }else{
-            if(this.products.length > 0){
-                const codeEncontrado = this.products.find(prod => prod.code === code)
-                if (codeEncontrado){
-                    console.log("El codigo ya existe")
-                }else{
-                    this.id = (this.products[this.products.length - 1].id + 1)
-                const product = {
-                    title,
-                    description,
-                    price,
-                    thumbnail,
-                    id: this.id,
-                    code,
-                    stock,}
-                this.products.push(product)
-                }
+            const codeEncontrado = this.products.find(prod => prod.code === code)
+            if (codeEncontrado){
+                console.log(`El codigo ${code} ya existe`)
             }else{
-                this.id = 1
+                ProductManager.id++
                 const product = {
                     title,
                     description,
                     price,
                     thumbnail,
+                    id: ProductManager.id,
                     code,
-                    id: this.id,
-                    stock,}
+                    stock,
+                }
                 this.products.push(product)
+                const productoString = JSON.stringify(this.products, null, 2)
+                await fs.promises.writeFile(this.path, productoString)
             }
         }
     }
-    getProductById(id){
-        const productEncontrado = this.products.find(prod => prod.id === id)
-        if(!productEncontrado){
-            console.log("Not found")
+    async getProductById(id){
+        let leerProductos = []
+        leerProductos = await this.getProducts()
+
+        const producto = leerProductos.find((prod) => prod.id === id)
+        if(producto){
+            console.log(producto)
         }else{
-            console.log(productEncontrado)
+            console.log("no se encontro")
+        }
+    }
+
+    async deleteProduct (id) {
+        const i = this.products.findIndex(elm => elm.id===id)
+
+        if (i === -1) {
+            return "Producto no encontrado";
+        } else {
+            const newProducts = this.products.filter((produc) => produc.id != id)
+            this.products = newProducts
+            const jsonProduct = JSON.stringify(this.products, null, 2)
+            await fs.promises.writeFile(this.path, jsonProduct)
+            return this.products
+        }
+    }
+
+    async updateProduct(id, data) {
+        let indice = this.products.findIndex(prod => prod.id === id);
+
+        if (indice !== -1) {
+            this.products[indice] = { ...this.products[indice], ...data, id };
+            const productoString = JSON.stringify(this.products, null, 2)
+            await fs.promises.writeFile(this.path, productoString)
+        } else {
+            console.log("Product not found");
         }
     }
 }
 
 const product = new ProductManager()
+product.addProduct("first item", "Product 5", 1000, "null", "eee",  3)
+product.addProduct("first item12", "Product 1", 1000, "null", "434",  3)
